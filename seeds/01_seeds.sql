6a7276280001681a73718126671a85025c2e035a
QuintonEL <quintonlaborde@gmail.com>
2020-02-05 01:32:50 +0000

    began creating seeds

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (name, email, password)
+VALUES ('Gimli', 'elffriend@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Aragorn', 'strider@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Legolas', 'greenleaf@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+INSERT INTO properties ()
